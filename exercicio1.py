1fd25bbbf78fd5a1374b319a022210ad4f005320
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-08-04 20:23:01 -0300

    Exercício

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+# 1. Faça um programa que peça uma nota, entre zero e dez. 
+# Mostre uma mensagem caso o valor seja inválido e continue 
+# pedindo até que o usuário informe um valor válido.
+
+nota = float(input("Digite um valor entre 0 e 10: "))
+while (nota>10) or (nota<0):
+    nota = float(input("Digite um valor entre 0 e 10: "))
