741ab5e37978102769c0e980542661530cf7ea7d
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-08-04 21:26:40 -0300

    Exercício

--- exercicio1.py
+++ exercicio1.py
@@ -1,6 +1,7 @@
 # 1. Faça um programa que peça uma nota, entre zero e dez. 
 # Mostre uma mensagem caso o valor seja inválido e continue 
 # pedindo até que o usuário informe um valor válido.
+# Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
 
 nota = float(input("Digite um valor entre 0 e 10: "))
 while (nota>10) or (nota<0):
--- exercicio2.py
+++ exercicio2.py
@@ -1,6 +1,7 @@
 # 2. Faça um programa que leia um nome de usuário e a sua senha 
 # e não aceite a senha igual ao nome do usuário, mostrando uma mensagem 
 # de erro e voltando a pedir as informações.
+# Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
 
 nome = input("Digite um nome de usuário: ")
 senha = input("Digite uma senha: ")
