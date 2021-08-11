3fc5ba1d9a67961979de40553ecd1227d5e6dfed
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-08-11 20:39:35 -0300

    Exercício

---
+++ contador.py
@@ -0,0 +1,5 @@
+# Contador
+
+count = 0
+for count in range(1, 101): # Aqui você define até que número será contado.
+    print(count)
--- exercicio15.py
+++ exercicio15.py
@@ -1,5 +1,19 @@
-# Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
+# 15. A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,...
+# Faça um programa capaz de gerar a série até o n−ésimo termo.
+# Fonte: https://www.pythonprogressivo.net/2018/06/Serie-Fibonacci-Como-Fazer-Sequencia.html
 
-count = 0
-for count in range(1, 2001):
-    print(count)
+n = int(input("Que termo deseja encontrar: "))
+ultimo=1
+penultimo=1
+
+
+if (n==1) or (n==2):
+    print("1")
+else:
+    count=3
+    while count <= n:
+        termo = ultimo + penultimo
+        penultimo = ultimo
+        ultimo = termo
+        count += 1
+    print(termo)
