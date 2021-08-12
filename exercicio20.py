54e4d1546f0d86dbfcdd771bd1a171a3cdfe1b41
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-08-12 13:00:30 -0300

    Exercício

--- exercicio20.py
+++ exercicio20.py
@@ -1,2 +1,5 @@
+# 20. Altere o programa de cálculo do fatorial, permitindo ao usuário 
+# calcular o fatorial várias vezes e limitando o fatorial a números 
+# inteiros positivos e menores que 16.
 # Desenvolvido por Jonathan Silveira - Instagram: @jonathandev01
 
