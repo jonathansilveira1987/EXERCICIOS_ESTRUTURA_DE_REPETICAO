6dbe362aaa0f708cfae71bb13475c24812f41bd0
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-08-13 16:59:19 -0300

    Exercício

--- contador.py
+++
@@ -1,6 +0,0 @@
-# Contador
-# Desenvolvido por Jonathan Silveira - Instagram: @jonathandev01
-
-count = 0
-for count in range(1, 101): # Aqui você define até que número será contado.
-    print(count)
